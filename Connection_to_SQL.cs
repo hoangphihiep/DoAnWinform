f68df5bc089f43c2bf08d4c1cd2b2b9d107e43cb
Khoa432004 <khoavt123456789@gmail.com>
2024-02-28 20:46:23 +0700

    Add sql

---
+++ Connection_to_SQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace DuLich
+{
+    public class Connection_to_SQL
+    {
+        private static string Connection = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=QLHocSinh;Integrated Security=True";
+
+        public static SqlConnection getConnection() 
+        {
+            return new SqlConnection(Connection);
+        }
+
+    }
+}
