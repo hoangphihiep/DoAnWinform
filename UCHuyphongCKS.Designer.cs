fdf8ac0061e061d7d2eed7567e4ac647714e5579
LiviasWS <22119119@student.hcmute.edu.vn>
2024-04-09 10:33:25 +0700

    chinh sua

---
+++ UCHuyphongCKS.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace DuLich
+{
+    partial class UCHuyphongCKS
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel10 = new Panel();
+            ptbImage = new PictureBox();
+            label1 = new Label();
+            btnConfirm = new Button();
+            panel11 = new Panel();
+            lblSoPhong = new Label();
+            label2 = new Label();
+            lblCusName = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            lblCheckOut = new Label();
+            lblPrice = new Label();
+            lblAddress = new Label();
+            label22 = new Label();
+            lblCheckIn = new Label();
+            lblName = new Label();
+            panel10.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ptbImage).BeginInit();
+            panel11.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel10
+            // 
+            panel10.BackColor = SystemColors.ControlLightLight;
+            panel10.Controls.Add(ptbImage);
+            panel10.Controls.Add(label1);
+            panel10.Controls.Add(btnConfirm);
+            panel10.Controls.Add(panel11);
+            panel10.Location = new Point(3, 3);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(724, 162);
+            panel10.TabIndex = 2;
+            // 
+            // ptbImage
+            // 
+            ptbImage.Location = new Point(14, 17);
+            ptbImage.Name = "ptbImage";
+            ptbImage.Size = new Size(188, 121);
+            ptbImage.TabIndex = 4;
+            ptbImage.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkTurquoise;
+            label1.Location = new Point(14, 141);
+            label1.Name = "label1";
+            label1.Size = new Size(687, 15);
+            label1.TabIndex = 3;
+            label1.Text = "----------------------------------------------------------------------------------------------------------------------------------------";
+            // 
+            // btnConfirm
+            // 
+            btnConfirm.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnConfirm.Location = new Point(601, 16);
+            btnConfirm.Name = "btnConfirm";
+            btnConfirm.Size = new Size(75, 23);
+            btnConfirm.TabIndex = 2;
+            btnConfirm.Text = "Confirm";
+            btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // panel11
+            // 
+            panel11.Controls.Add(lblSoPhong);
+            panel11.Controls.Add(label2);
+            panel11.Controls.Add(lblCusName);
+            panel11.Controls.Add(label4);
+            panel11.Controls.Add(label3);
+            panel11.Controls.Add(lblCheckOut);
+            panel11.Controls.Add(lblPrice);
+            panel11.Controls.Add(lblAddress);
+            panel11.Controls.Add(label22);
+            panel11.Controls.Add(lblCheckIn);
+            panel11.Controls.Add(lblName);
+            panel11.Location = new Point(208, 16);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(387, 122);
+            panel11.TabIndex = 1;
+            // 
+            // lblSoPhong
+            // 
+            lblSoPhong.AutoSize = true;
+            lblSoPhong.Location = new Point(80, 96);
+            lblSoPhong.Name = "lblSoPhong";
+            lblSoPhong.Size = new Size(13, 15);
+            lblSoPhong.TabIndex = 13;
+            lblSoPhong.Text = "1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(10, 96);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 15);
+            label2.TabIndex = 12;
+            label2.Text = "Số phòng: ";
+            // 
+            // lblCusName
+            // 
+            lblCusName.AutoSize = true;
+            lblCusName.Location = new Point(87, 78);
+            lblCusName.Name = "lblCusName";
+            lblCusName.Size = new Size(30, 15);
+            lblCusName.TabIndex = 11;
+            lblCusName.Text = "NVA";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(8, 78);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Khách hàng:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.DarkTurquoise;
+            label3.Location = new Point(165, 36);
+            label3.Name = "label3";
+            label3.Size = new Size(29, 15);
+            label3.TabIndex = 9;
+            label3.Text = "đến";
+            // 
+            // lblCheckOut
+            // 
+            lblCheckOut.AutoSize = true;
+            lblCheckOut.Location = new Point(219, 36);
+            lblCheckOut.Name = "lblCheckOut";
+            lblCheckOut.Size = new Size(140, 15);
+            lblCheckOut.TabIndex = 8;
+            lblCheckOut.Text = "Thứ 3, ngày 12 thg 3 2024";
+            // 
+            // lblPrice
+            // 
+            lblPrice.AutoSize = true;
+            lblPrice.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPrice.ForeColor = SystemColors.MenuHighlight;
+            lblPrice.Location = new Point(276, 91);
+            lblPrice.Name = "lblPrice";
+            lblPrice.Size = new Size(98, 17);
+            lblPrice.TabIndex = 6;
+            lblPrice.Text = "2.643.860 VNĐ";
+            // 
+            // lblAddress
+            // 
+            lblAddress.AutoSize = true;
+            lblAddress.Location = new Point(8, 51);
+            lblAddress.Name = "lblAddress";
+            lblAddress.Size = new Size(175, 15);
+            lblAddress.TabIndex = 5;
+            lblAddress.Text = "Nha Trang, Khánh Hòa Province";
+            // 
+            // label22
+            // 
+            label22.AutoSize = true;
+            label22.Location = new Point(13, 78);
+            label22.Name = "label22";
+            label22.Size = new Size(0, 15);
+            label22.TabIndex = 4;
+            // 
+            // lblCheckIn
+            // 
+            lblCheckIn.AutoSize = true;
+            lblCheckIn.Location = new Point(8, 36);
+            lblCheckIn.Name = "lblCheckIn";
+            lblCheckIn.Size = new Size(140, 15);
+            lblCheckIn.TabIndex = 1;
+            lblCheckIn.Text = "Thứ 3, ngày 12 thg 3 2024";
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblName.Location = new Point(8, 15);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(263, 21);
+            lblName.TabIndex = 0;
+            lblName.Text = "Melia Vinpearl Nha Trang Empire";
+            // 
+            // UCHuyphongCKS
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel10);
+            Name = "UCHuyphongCKS";
+            Size = new Size(752, 172);
+            Load += UCHuyphongCKS_Load;
+            panel10.ResumeLayout(false);
+            panel10.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)ptbImage).EndInit();
+            panel11.ResumeLayout(false);
+            panel11.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel10;
+        private Label label1;
+        private Button btnConfirm;
+        private Panel panel11;
+        private Label lblPrice;
+        private Label lblAddress;
+        private Label label22;
+        private Label lblCheckIn;
+        private Label lblName;
+        private PictureBox ptbImage;
+        private Label label3;
+        private Label lblCheckOut;
+        private Label lblCusName;
+        private Label label4;
+        private Label label2;
+        private Label lblSoPhong;
+    }
+}
---
+++ UCHuyphongCKS.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuLich
+{
+    public partial class UCHuyphongCKS : UserControl
+    {
+        KhachHang kh;
+        KHACHSAN ks;
+        Room phong;
+        DatPhong dp;
+        QL_HinhAnh QL_HinhAnh;
+        public UCHuyphongCKS(KhachHang kh, KHACHSAN ks, Room phong)
+        {
+            InitializeComponent();
+            this.kh = kh;
+            this.ks = ks;
+            this.phong = phong;
+        }
+
+        private void UCHuyphongCKS_Load(object sender, EventArgs e)
+        {
+            lblName.Text = ks.TENKS;
+            lblCheckIn.Text = dp.NgayNhan.Date.ToString();
+            lblCheckOut.Text = dp.NgayTra.Date.ToString();
+            lblAddress.Text = ks.DIACHI;
+            lblCusName.Text = kh.Ten;
+            lblSoPhong.Text = dp.SoPhong.ToString();
+            List<QL_HinhAnh> list = (new QL_HinhAnhDAO().Get(ks.MAKS));
+            foreach (QL_HinhAnh anh in list)
+            {
+                if (anh.TENANH == "anh chinh")
+                {
+                    ptbImage.Image = Image.FromFile(anh.ADDRESS);
+                }
+            }
+        }
+    }
+}
